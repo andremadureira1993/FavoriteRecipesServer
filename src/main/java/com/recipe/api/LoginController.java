a2a1c417af4d266700a91c6ed30f0a5c3147201c
André Moraes <andre_moraes@hst.com.br>
2021-06-01 01:19:30 -0300

    Added cors implementation

--- src/main/java/com/recipe/api/LoginController.java
+++ src/main/java/com/recipe/api/LoginController.java
@@ -1,6 +1,8 @@
 package com.recipe.api;
 
 import javax.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -13,11 +15,14 @@ import com.recipe.services.LoginService;
 @Controller
 public class LoginController implements LoginApi {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
+
     @Autowired
     private LoginService authService;
 
     @Override
     public ResponseEntity<LoginResponse> login(@Valid LoginRequest loginRequest) {
+        LOGGER.info("Processing login for user: " + loginRequest.getUsername());
         return ResponseEntity.ok(authService.authenticate(loginRequest));
     }
 }
--- src/main/java/com/recipe/configurations/CorsConfiguration.java
+++ src/main/java/com/recipe/configurations/CorsConfiguration.java
@@ -1,20 +1,19 @@
-//package com.recipe.configurations;
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class CorsConfiguration implements WebMvcConfigurer {
-//    private static final Logger LOGGER = LoggerFactory.getLogger(CorsConfiguration.class);
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        LOGGER.info("Using HST Consumer Controls CORS configuration");
-//        registry.addMapping("/**")
-//            .allowedHeaders("Authorization", "Content-Type", "institutionCode", "accountId", "userId", "requestID")
-//            .allowedMethods("GET", "POST", "PUT", "DELETE");
-//    }
-//}
+package com.recipe.configurations;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CorsConfiguration.class);
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedHeaders("Authorization", "Content-Type")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
